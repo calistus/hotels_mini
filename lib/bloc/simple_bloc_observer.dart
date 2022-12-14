37b532e51e053d9f2dc45edcb48310f86ac33c40
Ilo Calistus <icalistus@gmail.com>
2022-12-14 09:28:13 +0100

    Add simple Bloc Observer and cleanup main.dart

---
+++ lib/bloc/simple_bloc_observer.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart' as foundation;
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/// [BlocObserver] for the application which
+/// observes all state changes.
+class BlocMonitor extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    foundation.debugPrint('${bloc.runtimeType} $change');
+  }
+}
