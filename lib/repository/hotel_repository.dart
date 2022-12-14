85d2573b78d0161ad2ff02bf3a04bdbbe8b6e35a
Ilo Calistus <icalistus@gmail.com>
2022-12-14 08:43:03 +0100

    Add hotel repository

--- lib/repository/hotel_repository.dart
+++ lib/repository/hotel_repository.dart
@@ -1,1 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:hotels_mini/config/api_config.dart';
+import 'package:hotels_mini/model/HotelModel.dart';
 
+class HotelRepository {
+  Future<HotelModel?> fetchHotels() async {
+    try {
+      Dio dio = Dio();
+      Response response = await dio.get(baseURL);
+      if (response.statusCode == 200) {
+        return HotelModel.fromJson(response.data);
+      }
+    } on DioError catch (e) {
+      APIConfig.getPlatformExceptionErrorResult(e);
+    }
+  }
+}
