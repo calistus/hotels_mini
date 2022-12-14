92c87611a7441e0ec286cd463cf2b4b6023267af
Ilo Calistus <icalistus@gmail.com>
2022-12-14 09:34:55 +0100

    Wrap App entry point with a Bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hotels_mini/bloc/hotels/hotels_bloc.dart';
+import 'package:hotels_mini/repository/hotel_repository.dart';
 import 'package:hotels_mini/ui/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'bloc/simple_bloc_observer.dart';
+
+Future<void> main() async {
+  return BlocOverrides.runZoned(
+    () async {
+      runApp(MyApp());
+    },
+    blocObserver: BlocMonitor(),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hotel Mini',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen(),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider<HotelsBloc>(
+            create: (context) =>
+                HotelsBloc(hotelRepository: HotelRepository()..fetchHotels()),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Hotel Mini',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomeScreen(),
+        ));
   }
 }
